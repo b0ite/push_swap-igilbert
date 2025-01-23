779ca8f59ef91fdaf088e1da3b811e74241edf42
Ivan Gilbert <igilbert@MacBook-Pro-de-Ivan.local>
2025-01-23 13:12:55 +0100

    auto update

--- push_swap.c
+++ push_swap.c
@@ -6,8 +6,24 @@
 /*   By: igilbert <igilbert@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/19 17:04:58 by igilbert          #+#    #+#             */
-/*   Updated: 2024/12/19 17:24:27 by igilbert         ###   ########.fr       */
+/*   Updated: 2025/01/23 13:12:48 by igilbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void	push_swap(t_list **list)
+{
+	int	max;
+	int	max_bit;
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	max = ft_lstsize(list) - 1;
+	max_bit = 0;
+	while (max >> max_bit)
+		max_bit++;
+	
+}
